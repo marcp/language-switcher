67629d3b8be37519b36dcdf73b9234a8dd9b9bd1
Marc Poris <marc.poris@healthsparq.com>
2017-09-20 15:15:45 -0700

    Cleanup the about-ness template.

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -2,7 +2,7 @@ export default {
   'about': {
     'main': {
       'text': 'Some text for the main about section.',
-      'moreText': 'And some more text, {{name}} for the main about section number {{sectionNumber}}.',
+      'moreText': 'And some more text, {{name}}, for the main about section number {{sectionNumber}}.',
       'withPluralization': {
         'zero': 'No dinosaurs',
         'one': 'A single dinosaur',
